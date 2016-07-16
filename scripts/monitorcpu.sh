9874a5d719bbf86f96aa8fdab47898d3bd5ce5b1
soldierxue <sys@soldierxue.cn>
2016-07-16 09:53:14 +0000

    add scripts

---
+++ scripts/monitorcpu.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+util=`uptime | awk '{print $9}' | awk -F ',' '{print $1}'`
+directory="/home/ec2-user/logs/"
+# 对应到 CPU 的总核数，比如 c4.4xlarge 就是 16 Core
+maxusage=0.01
+dt=`date "+%Y-%m-%d-%H:%M:%S"`
+if [[ `expr $util \> $maxusage` -eq 1 ]] ; then
+ uptime > ${directory}${dt}"cpuload.txt"
+ top -b -n 1 > ${directory}${dt}"processinfo.txt"
+ netstat -anpt > ${directory}${dt}"networkinfo.txt"
+fi
