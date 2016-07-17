6339dccdd79e0e733eaee8c927f48ef5a0f2d20c
soldierxue <sys@soldierxue.cn>
2016-07-17 15:56:51 +0800

    Update monitorcpu.sh

--- scripts/monitorcpu.sh
+++ scripts/monitorcpu.sh
@@ -2,7 +2,7 @@
 util=`uptime | awk '{print $10}' | awk -F ',' '{print $1}'`
 directory="/home/ec2-user/logs/"
 # 对应到 CPU 的总核数，比如 c4.4xlarge 就是 16 Core
-maxusage=15
+maxusage=3.5
 dt=`date "+%Y-%m-%d-%H:%M:%S"`
 if [[ `expr $util \> $maxusage` -eq 1 ]] ; then
  uptime > ${directory}${dt}"cpuload.txt"
